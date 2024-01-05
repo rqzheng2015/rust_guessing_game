3728dafaf6b926e1925b5ae34a0783338ab53c40
Rickie Zheng <rqzheng2008@126.com>
2024-01-05 17:45:59 +0800

    feat: init project

---
+++ src/main.rs
@@ -0,0 +1,36 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
+fn main() {
+    println!("Guess the number!");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    println!("The secret number is: {secret_number}");
+
+    loop {
+        println!("Please input your guess. Its range is from 0 to 100.");
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
+
+        println!("You guessed: {guess}");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        if guess < 0 || guess > 100 {
+            println!("Invalid input. Please enter a number between 0 and 100.");
+            continue;
+        }
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small, please retry."),
+            Ordering::Greater => println!("Too big, please retry."),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
+}
